bf87770f17ae6e5e22ffe322f0dd91c46a44e69a
Toshiya Takeshita <toshi@atbat.jp>
2022-07-22 10:57:00 +0900

    pickleのサンプル追加

---
+++ Pickle.py
@@ -0,0 +1,21 @@
+import pickle
+
+# x = [10, 20, 30]
+
+# with open('./data.pickle', 'wb') as f:
+#     pickle.dump(x, f)
+
+# with open('./data.pickle', 'rb') as f:
+#     x = pickle.load(f)
+# print(x)
+
+from DataClass import User
+
+user = User('Suzuki', 21)
+with open('./data.pickle', 'wb') as f:
+    pickle.dump(user, f)
+
+with open('./data.pickle', 'rb') as f:
+    user = pickle.load(f)
+
+print(user)
