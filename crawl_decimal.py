f854974bde2a349736414f37e195d7ad6a535bc3
Toshiya Takeshita <toshi@atbat.jp>
2023-04-06 21:30:18 +0900

    1割る9801について再

--- crawl_decimal.py
+++ crawl_decimal.py
@@ -1,6 +1,10 @@
 from decimal import Decimal
 from decimal import getcontext
+"""
+巡回小数の 1 / 9801 を求める
+https://blog.tstylestudio.com/2023/04/06/1%e3%82%929801%e3%81%a7%e5%89%b2%e3%81%a3%e3%81%9f%e8%a8%88%e7%ae%97%e7%b5%90%e6%9e%9c/
 
+"""
 getcontext().prec = 250
 
 _miracle_number = Decimal('1') / Decimal('9801')
